10d8e492dd05cd9cf6a3bc60e17e708bd42675f1
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-09-14 14:25:51 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  word.scan(/\b[aeiouAEIOU]\w*\b/) != []
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.scan(/(^[A-Z]).*([.?!]$)/) != []
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/(\d+|\()\d+(\s|\)|-|\d)\d+(\s|\)|-|\d)\d+/) != []    #/[(]*(\d+)[)\s]*(\d+)[-\s]*(\d+)/
 end
