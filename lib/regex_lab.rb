456d054574a803413b5c7e37611b527b5e62bcb3
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-09-14 14:25:25 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/[(]*(\d+)[)\s]*(\d+)[-\s]*(\d+)/) != []
+  phone.scan(/(\d+|\()\d+(\s|\)|-|\d)\d+(\s|\)|-|\d)\d+/) != []    #/[(]*(\d+)[)\s]*(\d+)[-\s]*(\d+)/
 end
